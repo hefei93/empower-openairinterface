23df28303bc23473f50b14d3f4cfe8a0a4c69c13
herlesupreeth <herlesupreeth@gmail.com>
2017-10-31 12:37:08 +0100

    FIFO based tenant scheduler implementation

---
+++ RAN_sharing/tenant_schedulers/fifo_t.h
@@ -0,0 +1,30 @@
+/* Copyright (c) 2016 Supreeth Herle <s.herle@create-net.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* Implementation of First In First Out based scheduling of radio resources
+ * among tenants in an eNB.
+ */
+
+#ifndef __FIFO_T_SCHED_H
+#define __FIFO_T_SCHED_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+/* Importing global variables and variable types defined in OAI */
+#include "ran_sharing_defs.h"
+
+#endif
